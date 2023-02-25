88d9ca357bd4a6da179e762046a812882230c086
corinneang <cang@risd.edu>
2023-02-25 01:00:19 -0500

    update

--- index-script.js
+++ index-script.js
@@ -229,17 +229,17 @@ const projectId = function () {
 	});
 };
 document.getElementById('construction-zone').onmouseover = projectId;
-////document.getElementById('sundae-flowers').onmouseover = projectId;
+//document.getElementById('sundae-flowers').onmouseover = projectId;
+document.getElementById('rug').onmouseover = projectId;
 document.getElementById('kilder').onmouseover = projectId;
 document.getElementById('cj-type').onmouseover = projectId;
-//document.getElementById('gretel-hanlyn').onmouseover = projectId;
 document.getElementById('spring-speaker').onmouseover = projectId;
 document.getElementById('flipbook').onmouseover = projectId;
 document.getElementById('yearbook').onmouseover = projectId;
 document.getElementById('poi').onmouseover = projectId;
 document.getElementById('barangay').onmouseover = projectId;
 document.getElementById('haunted').onmouseover = projectId;
-document.getElementById('road-tripping').onmouseover = projectId;
+//document.getElementById('road-tripping').onmouseover = projectId;
 
 
 
